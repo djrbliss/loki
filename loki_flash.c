eb49b54363d1a6b630d69c693aef48328926b004
Dan Rosenberg <dan.j.rosenberg@gmail.com>
2013-05-23 10:47:39 -0400

    Whitespace cleanup

--- loki_flash.c
+++ loki_flash.c
@@ -21,20 +21,20 @@
 
 struct boot_img_hdr
 {
-    unsigned char magic[BOOT_MAGIC_SIZE];
-    unsigned kernel_size;  /* size in bytes */
-    unsigned kernel_addr;  /* physical load addr */
-    unsigned ramdisk_size; /* size in bytes */
-    unsigned ramdisk_addr; /* physical load addr */
-    unsigned second_size;  /* size in bytes */
-    unsigned second_addr;  /* physical load addr */
-    unsigned tags_addr;    /* physical addr for kernel tags */
-    unsigned page_size;    /* flash page size we assume */
-    unsigned dt_size;      /* device_tree in bytes */
-    unsigned unused;    /* future expansion: should be 0 */
-    unsigned char name[BOOT_NAME_SIZE]; /* asciiz product name */
-    unsigned char cmdline[BOOT_ARGS_SIZE];
-    unsigned id[8]; /* timestamp / checksum / sha1 / etc */
+	unsigned char magic[BOOT_MAGIC_SIZE];
+	unsigned kernel_size;	/* size in bytes */
+	unsigned kernel_addr;	/* physical load addr */
+	unsigned ramdisk_size;	/* size in bytes */
+	unsigned ramdisk_addr;	/* physical load addr */
+	unsigned second_size;	/* size in bytes */
+	unsigned second_addr;	/* physical load addr */
+	unsigned tags_addr;		/* physical addr for kernel tags */
+	unsigned page_size;		/* flash page size we assume */
+	unsigned dt_size;		/* device_tree in bytes */
+	unsigned unused;		/* future expansion: should be 0 */
+	unsigned char name[BOOT_NAME_SIZE];	/* asciiz product name */
+	unsigned char cmdline[BOOT_ARGS_SIZE];
+	unsigned id[8];			/* timestamp / checksum / sha1 / etc */
 };
 
 struct loki_hdr
