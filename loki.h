907d89e6926755fcb9c62a75d883b82500bab2fd
philz-cwm6 <phytowardt@gmail.com>
2013-12-06 15:54:27 +0100

    remove licence from header

--- loki.h
+++ loki.h
@@ -1,18 +1,5 @@
-/*
- * By Seth Shelnutt
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+#ifndef __LOKI_H_
+#define __LOKI_H_
 
 #define VERSION "2.1"
 
@@ -59,3 +46,5 @@ struct loki_hdr {
     unsigned int orig_ramdisk_size;
     unsigned int ramdisk_addr;
 };
+
+#endif //__LOKI_H_
